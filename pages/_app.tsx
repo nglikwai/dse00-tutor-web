e8e6916d65fbf3defc8ea5a09ca4fac254e449cc
alexlamazing <alexlamazing@users.noreply.github.com>
2022-04-15 16:30:43 +0800

    chore: setup (#1)
    * chore: update tsconfig
    
    * chore: config babel
    
    * chore: config eslint
    
    * chore: update husky
    
    * chore: add styled-component, ramda

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  // custom theme
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
