c309f633af8377b0e1cdc72cf4a665bf712f4c95
alexlamazing <alexlamazinga@gmail.com>
2022-05-09 21:57:32 +0800

    fix: action typing

--- src/redux/search/saga/searchTutorWorker.ts
+++ src/redux/search/saga/searchTutorWorker.ts
@@ -1,9 +1,20 @@
+import { PayloadAction } from '@reduxjs/toolkit'
 import { call, put } from 'redux-saga/effects'
 import { searchTutorSuccess } from 'src/redux/search'
 import { Tutor } from 'src/types'
 
-export function* searchTutorWorker(action: any) {
-  console.log(action)
+import { SearchTutorRequestPayload } from '../types'
+
+export function* searchTutorWorker(
+  action: PayloadAction<SearchTutorRequestPayload>,
+) {
+  const {
+    payload: { place, subject },
+  } = action
+
+  console.log('place: ', place)
+
+  console.log('subject: ', subject)
 
   try {
     const response = (yield call(() =>
